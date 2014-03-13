6e681abd482f6005e18c4e8a59455a12a220611a
Rodja Trappe <mail@rodja.net>
2014-03-13 11:08:59 +0100

    experimenting with desktop project

---
+++ Desktop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Desktop
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
