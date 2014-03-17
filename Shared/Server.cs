51baf91f5a3e6bc82066b7bc5a04b368fe4c4bd1
Falko Schindler <mail@falkoschindler.de>
2014-03-17 11:46:46 +0100

    started to implement server class. simple get command already working

---
+++ Shared/Server.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+namespace PerpetualEngine
+{
+    public class Server
+    {
+        private string Request(string method, string path)
+        {
+            var request = HttpWebRequest.Create(path);
+            request.ContentType = "application/json";
+            request.Method = method;
+            try {
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse) {
+                    if (response.StatusCode != HttpStatusCode.OK)
+                        Console.WriteLine("Error fetching data. Server returned status code: {0}", response.StatusCode);
+                    using (StreamReader reader = new StreamReader(response.GetResponseStream())) {
+                        var content = reader.ReadToEnd();
+                        if (string.IsNullOrWhiteSpace(content)) {
+                            Console.WriteLine("Response contained empty body...");
+                        } else {
+                            Console.WriteLine("Response Body: \r\n {0}", content);
+                            return content;
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                Console.WriteLine(e.Message);
+            }
+
+            return null;
+        }
+
+        public string DownloadString(string path)
+        {
+            return Request("GET", path);
+        }
+    }
+}
+
---
+++ Shared/ServerTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace PerpetualEngine
+{
+    [TestFixture()]
+    public class ServerTests
+    {
+        [Test()]
+        public void TestDownloadString()
+        {
+            var server = new Server();
+            var url = "http://tinyurl.com/api-create.php?url=http://www.perpetual-mobile.de/";
+            var response = server.DownloadString(url);
+            Assert.That(response, Is.EqualTo("http://tinyurl.com/pcyutsv"));
+        }
+    }
+}
+    
