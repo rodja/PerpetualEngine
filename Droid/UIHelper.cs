a3ca570aa2b82d687df0ad3006790e74bf92c5d8
Rodja Trappe <mail@rodja.net>
2014-04-14 11:25:56 +0200

    added dpToPixels helper function

---
+++ Droid/UIHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Android.Util;
+using Android.Content;
+
+namespace Droid
+{
+    public class UIHelper
+    {
+        public static int DpToPixels(Context context, int dp)
+        {
+            return (int)TypedValue.ApplyDimension(ComplexUnitType.Dip, dp, context.Resources.DisplayMetrics);
+        }
+    }
+}
+
