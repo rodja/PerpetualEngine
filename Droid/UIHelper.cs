a1c7d3158953868714f5500fee334cdc0aad5693
kaolick <kaolick@gmail.com>
2014-04-30 10:11:49 +0200

    Added context to UIHelper.

--- Droid/UIHelper.cs
+++ Droid/UIHelper.cs
@@ -2,12 +2,21 @@
 using Android.Util;
 using Android.Content;
 
-namespace Droid
+namespace PerpetualEngine.Droid
 {
     public class UIHelper
     {
-        public static int DpToPixels(Context context, int dp)
+        static Context context;
+
+        public static void SetContext(Context context)
         {
+            UIHelper.context = context;
+        }
+
+        public static int DpToPixels(int dp)
+        {
+            if (context == null)
+                throw new InvalidOperationException("please provide a context");
             return (int)TypedValue.ApplyDimension(ComplexUnitType.Dip, dp, context.Resources.DisplayMetrics);
         }
     }
