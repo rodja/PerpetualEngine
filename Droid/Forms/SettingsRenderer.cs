3d9ada1632dca7729b78a26f273edb86e676cd3f
Rodja Trappe <mail@rodja.net>
2014-06-22 16:55:11 +0200

    compiles agian

---
+++ Droid/Forms/SettingsRenderer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.Forms.Platform.Android;
+using Android.Content;
+using Xamarin.Forms;
+using Android.Views;
+using Android.App;
+
+namespace PerpetualEngine.Forms
+{
+    abstract public class SettingsRenderer :ViewCellRenderer
+    {
+        protected override global::Android.Views.View GetCellCore(Cell item, global::Android.Views.View convertView, ViewGroup parent, Context context)
+        {
+            var view = base.GetCellCore(item, convertView, parent, context) as ViewGroup;
+
+            (item as Setting).RendererTapAction = delegate {
+                var dialog = CreateDialog(context);
+                dialog.Window.SetSoftInputMode(SoftInput.StateVisible);
+                dialog.Show();
+            };
+            return view;
+        }
+
+        abstract protected Dialog CreateDialog(Context context);
+    }
+}
+
