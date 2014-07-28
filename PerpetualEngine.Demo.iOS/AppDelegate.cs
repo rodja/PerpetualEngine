e508cd65a694e0dc2e0e3dd5c7fa4ee648c51366
Falko Schindler <mail@falkoschindler.de>
2014-07-28 07:24:24 +0200

    created iOS demo project. not yet working when app contains one of our new settings elements

---
+++ PerpetualEngine.Demo.iOS/AppDelegate.cs
@@ -0,0 +1,24 @@
+﻿using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using Xamarin.Forms;
+
+namespace PerpetualEngine.Demo.iOS
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : UIApplicationDelegate
+    {
+        UIWindow window;
+
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init();
+
+            window = new UIWindow(UIScreen.MainScreen.Bounds);
+            window.RootViewController = App.GetMainPage().CreateViewController();
+            window.MakeKeyAndVisible();
+            
+            return true;
+        }
+    }
+}
+
