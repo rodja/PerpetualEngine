fa9a5c03e313cad5b92f35b4e69eec3719759404
Falko Schindler <mail@falkoschindler.de>
2014-07-31 18:14:52 +0200

    implemented text setting on iOS

--- PerpetualEngine.Forms.Shared/TextSetting.cs
+++ PerpetualEngine.Forms.Shared/TextSetting.cs
@@ -1,9 +1,24 @@
+using Xamarin.Forms;
+
 namespace PerpetualEngine.Forms
 {
     public class TextSetting: Setting
     {
         public TextSetting(string title, string key = null) : base(title, key)
         {
+            if (Device.OS == TargetPlatform.iOS) {
+                Title.IsVisible = false;
+                Description.WidthRequest = 0;
+                var entry = new Entry {
+                    Placeholder = Title.Text,
+                    Text = Value,
+                    HorizontalOptions = LayoutOptions.FillAndExpand,
+                };
+                entry.Completed += delegate {
+                    Value = entry.Text;
+                };
+                (View as StackLayout).Children.Add(entry);
+            }
         }
     }
 }
