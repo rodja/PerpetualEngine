e619e6a95defae40ba2d809e65482fa2655c3319
Falko Schindler <mail@falkoschindler.de>
2014-08-14 08:27:50 +0200

    first experiment with additional entry on Android selection dialog

--- PerpetualEngine.Forms.Droid/SelectionSettingsRenderer.cs
+++ PerpetualEngine.Forms.Droid/SelectionSettingsRenderer.cs
@@ -6,6 +6,9 @@ using Android.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 using PerpetualEngine.Forms;
+using Android.Widget;
+using System;
+using Android.OS;
 
 [assembly: ExportRenderer(typeof(SelectionSetting), typeof(SelectionSettingsRenderer))]
 
@@ -13,7 +16,7 @@ namespace PerpetualEngine.Forms
 {
     public class SelectionSettingsRenderer : ViewCellRenderer
     {
-        protected override global::Android.Views.View GetCellCore(Cell item, global::Android.Views.View convertView, ViewGroup parent, Context context)
+        protected override Android.Views.View GetCellCore(Cell item, Android.Views.View convertView, ViewGroup parent, Context context)
         {
             (item as Setting).TapAction = delegate {
                 ShowSelectionDialog(context);
@@ -28,23 +31,48 @@ namespace PerpetualEngine.Forms
             var builder = new AlertDialog.Builder(context);
             builder.SetTitle(setting.Title.Text);
             var options = setting.Options;
-            builder.SetItems(options.Values.ToArray<string>(), new SelectionApplier {
-                Values = options.Keys.ToList<string>(),
-                Setting = setting
-            });
-            builder.Create().Show();
-        }
-
-        public class SelectionApplier: Java.Lang.Object, IDialogInterfaceOnClickListener
-        {
-            public List<string> Values;
-            public SelectionSetting Setting;
-
-            public void OnClick(IDialogInterface dialog, int which)
-            {
-                Setting.Value = Values[which];
+            AlertDialog dialog = null;
+            var viewGroup = new TableLayout(context);
+            var editText = new EditText(context);
+            viewGroup.AddView(editText);
+            for (var i = 0; i < options.Values.Count; i++) {
+                var view = new TextView(context) {
+                    Text = options.Values.ElementAt(i),
+                    Tag = options.Keys.ElementAt(i),
+                    TextSize = 20,
+                };
+                view.SetPadding(16, 16, 16, 16);
+                viewGroup.AddView(view);
+                view.Touch += (sender, e) => {
+                    Console.WriteLine(view.Text + " clicked");
+                    view.SetBackgroundColor(Android.Graphics.Color.DarkGray);
+                    setting.Value = view.Tag.ToString();
+                    (new Handler()).PostDelayed(delegate {
+                        dialog.Dismiss();
+                    }, 100);
+                };
             }
+            builder.SetView(viewGroup);
+
+//            builder.SetItems(options.Values.ToArray<string>(), new SelectionApplier {
+//                Values = options.Keys.ToList<string>(),
+//                Setting = setting
+//            });
+
+            dialog = builder.Create();
+            dialog.Show();
         }
+
+        //        public class SelectionApplier: Java.Lang.Object, IDialogInterfaceOnClickListener
+        //        {
+        //            public List<string> Values;
+        //            public SelectionSetting Setting;
+        //
+        //            public void OnClick(IDialogInterface dialog, int which)
+        //            {
+        //                Setting.Value = Values[which];
+        //            }
+        //        }
 
     }
 }
