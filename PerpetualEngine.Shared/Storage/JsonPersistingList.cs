25cbad09689b29facfd552eaee149aaedd2c6f76
Rodja Trappe <mail@rodja.net>
2014-11-18 16:02:48 +0100

    catching all execptions which can happen while dezerializing (eg. ArgumentNullEx..)

--- PerpetualEngine.Shared/Storage/JsonPersistingList.cs
+++ PerpetualEngine.Shared/Storage/JsonPersistingList.cs
@@ -6,20 +6,20 @@ namespace PerpetualEngine.Storage
     public class JsonPersistingList<T> : PersistentList<T> where T : IIdentifiable
     {
         public JsonPersistingList(string editGroup) : base(
-            editGroup, 
-            (obj) => {
-                var str = JsonConvert.SerializeObject(obj);
-                return str;
-            },
-            (str) => {
-                try {
-                    return JsonConvert.DeserializeObject<T>(str);
-                } catch (JsonException e) {
-                    Console.WriteLine("could not deserialize '" + str + "'");
-                    Console.WriteLine(e.GetType().ToString() + ": " + e.Message);
-                    return default(T);
-                }
-            })
+                editGroup, 
+                (obj) => {
+                    var str = JsonConvert.SerializeObject(obj);
+                    return str;
+                },
+                (str) => {
+                    try {
+                        return JsonConvert.DeserializeObject<T>(str);
+                    } catch (Exception e) {
+                        Console.WriteLine("could not deserialize '" + str + "'");
+                        Console.WriteLine(e.GetType().ToString() + ": " + e.Message);
+                        return default(T);
+                    }
+                })
         {
         }
     }
